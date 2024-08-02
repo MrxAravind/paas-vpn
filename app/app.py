317c8947f93061b1ff8f46b8558620593235d82e
mrspidy <74917158+MrxAravind@users.noreply.github.com>
2024-08-02 12:55:21 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,9 +1,11 @@
+import requests 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1><p style='font-family:monospace'>This VPN service is running from <a href='https://github.com/devolart/paas-vpn'>this source code</a></p></h1>"
+    myip = requests.get("http://myexternalip.com/raw").text
+    return f"<h1><p style='font-family:monospace'>This is My IP {myip}</p></h1><br><h1><p style='font-family:monospace'>This VPN service is running from <a href='https://github.com/MrxAravind/paas-vpn'>this source code</a></p></h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
